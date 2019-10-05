5a933e589e17505772e51c3a5d3d54da1b5fe013
Nick Hodges <nickhodges@gmail.com>
2019-10-05 08:21:34 -0400

    Guard, navigate

--- src/app/authGuard.ts
+++ src/app/authGuard.ts
@@ -2,6 +2,6 @@ import { CanActivate } from '@angular/router/src/utils/preactivation';
 
 export class AuthGuard implements CanActivate {
   canActivate() {
-    return true;  
+    return true;
   }
 }
