fc8ee035504ce53ca0f95a96675c8783d15c6b8c
Nick Hodges <nickhodges@gmail.com>
2019-10-06 20:02:15 -0400

    Query Params

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
 
-  constructor() { }
+export class ProductComponent implements OnInit {
+  prodType: string;
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.queryParamMap.subscribe((params: ParamMap) => {
+      this.prodType = params.get('prodtype');
+    });
   }
 
+  goProducts() {
+    this.router.navigate(['/product'], { queryParams: { prodtype: 'popular' } })
+  }
 }
